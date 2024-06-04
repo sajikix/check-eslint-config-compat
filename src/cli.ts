c5542fa42093d48efbb4f6ba21a3fac717183d95
saji-ryu <rsajiki@gmail.com>
2024-06-04 10:55:02 +0900

    fix: delete unnecessary code

--- src/cli.ts
+++ src/cli.ts
@@ -1,23 +1,8 @@
 import { Command } from "commander";
-// import { checkConfigCompatibility } from "./checker";
 import { generateOldConfigCompatData } from "./generateOldConfigCompatData";
 import { compareWithCompatInfo } from "./compareWithCompatInfo";
 
 const program = new Command();
-
-// program
-//   .command("compare")
-//   .option("-o, --old <string>", "Path for old eslint config")
-//   .option("-n, --new <string>", "Path for new eslint config")
-//   .option(
-//     "-e, --ext <string>",
-//     "File extensions to apply lint. (like: 'js,ts,jsx,tsx')",
-//   )
-//   .action((options) => {
-//     checkConfigCompatibility(options.old || ".", options.new || ".", {
-//       extensions: options.ext,
-//     });
-//   });
 
 program
   .command("extract")
--- test/eslint.config.test.js
+++
@@ -1,1 +0,0 @@
-module.exports = import("./eslint.config.test.mjs").then((n) => n.default);
